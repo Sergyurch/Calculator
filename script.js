2c5cea9c57c56eb0cb262d560bac3cdda793e81b
Sergej Jurchenko <sergyurch@gmail.com>
2019-01-22 15:58:00 +0000

    Add new functions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const calculator = document.getElementById('calculator');
 const mainScreen = document.getElementById('main_screen');
 const actionsScreen = document.getElementById('actions_screen');
 let memory = null;
+let backspaceIsAllowed = true;
 
 calculator.addEventListener('click', function(e) {
 	switch(e.target.id) {
@@ -16,23 +17,25 @@ calculator.addEventListener('click', function(e) {
     	case '7':
     	case '8':
     	case '9':
+	    	backspaceIsAllowed = true;
 	    	if (mainScreen.textContent == '0') {
 	    		mainScreen.textContent = e.target.id;
 	    	} else if (mainScreen.textContent.length < 12) {
 	    		mainScreen.textContent += e.target.id;
 	    	} else if (mainScreen.textContent.length == 12 && 
 			    	  (mainScreen.textContent[0] == '-' || mainScreen.textContent.indexOf('.') != -1) ) {
-	    		console.log('fdsf');
-                mainScreen.textContent += e.target.id;
+	    		mainScreen.textContent += e.target.id;
 	    	} else if (mainScreen.textContent.length == 13 && 
 	    		       mainScreen.textContent[0] == '-' && mainScreen.textContent.indexOf('.') != -1) {
                 mainScreen.textContent += e.target.id;
 	    	}
 	    	break;
 	    case 'dot':
+		    backspaceIsAllowed = true;
 		    if (mainScreen.textContent.indexOf('.') == -1) mainScreen.textContent += '.';
 		    break;
 	    case 'sign':
+		    backspaceIsAllowed = true;
 		    if (mainScreen.textContent != '0' && mainScreen.textContent[0] != '-') {
 		    	//if (mainScreen.textContent.length >= 12) mainScreen.style.fontSize = '56px';
 		    	mainScreen.textContent = '-' + mainScreen.textContent;
@@ -42,25 +45,38 @@ calculator.addEventListener('click', function(e) {
 		    }
 		    break;
 	    case 'backspace':
-		    if (mainScreen.textContent.length == 1 || (mainScreen.textContent.length == 2 && mainScreen.textContent[0] == '-') ) {
-		    	mainScreen.textContent = 0;
-		    } else {
-			    mainScreen.textContent = mainScreen.textContent.slice(0, mainScreen.textContent.length - 1);
+		    if (backspaceIsAllowed == true) {
+			    if (mainScreen.textContent.length == 1 || (mainScreen.textContent.length == 2 && mainScreen.textContent[0] == '-') ) {
+			    	mainScreen.textContent = 0;
+			    } else {
+				    mainScreen.textContent = mainScreen.textContent.slice(0, mainScreen.textContent.length - 1);
+			    }
+			    if (mainScreen.textContent.length > 12) mainScreen.style.fontSize = '58px';
 		    }
-		    if (mainScreen.textContent.length > 12) mainScreen.style.fontSize = '58px';
 		    break;
 	    case 'c':
+		    backspaceIsAllowed = true;
 		    mainScreen.textContent = 0;
+		    actionsScreen.textContent = '';
 		    break;
 	    case 'm_plus':
+		    backspaceIsAllowed = false;
 		    memory = +mainScreen.textContent;
 		    break;
 	    case 'mr':
+		    backspaceIsAllowed = false;
 		    mainScreen.textContent = memory;
 		    break;
 	    case 'root':
+		    backspaceIsAllowed = false;
+		    actionsScreen.textContent = `sqrt(${mainScreen.textContent})`;
 		    mainScreen.textContent = root(mainScreen.textContent);
 		    if (mainScreen.textContent >= 0.99999999999 && mainScreen.textContent <= 1.00000000001) mainScreen.textContent = 1;
+		    break;
+	    case 'fraction':
+		    backspaceIsAllowed = false;
+		    actionsScreen.textContent = `fraction(${mainScreen.textContent})`;
+		    mainScreen.textContent = fraction(mainScreen.textContent);
 		    break;
 
     }
