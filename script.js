a79fb566e8ccd1587e5d2285da60bca70c91e32e
Sergej Jurchenko <sergyurch@gmail.com>
2019-01-27 16:25:48 +0000

    Done

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ let action = null;
 let cleanScreen = true;
 let firstPress = true;
 screen.textContent = result;
-document.onkeydown = (e) => console.log(e.key);
-
 
 calculator.addEventListener('click', function(e) {
 	switch(e.target.id) {
@@ -27,71 +25,38 @@ calculator.addEventListener('click', function(e) {
     	case '8':
     	case '9':
 	    	backspaceIsAllowed = true;
-	    	//b2 = null;
-
 	    	if (cleanScreen == true || screen.textContent == '0') {
 	    		screen.textContent = result = e.target.id;
 	    		cleanScreen = false;
-	    		console.log(result);
 	    	} else if (screen.textContent.length < 12) {
 	    		screen.textContent = result += e.target.id;
-	    		console.log(result);
 	    	} else if (screen.textContent.length == 12 && 
 			    	  (screen.textContent[0] == '-' || screen.textContent.indexOf('.') != -1) ) {
 	    		screen.textContent = result += e.target.id;
-	    		console.log(result);
 	    	} else if (screen.textContent.length == 13 && 
 	    		       screen.textContent[0] == '-' && screen.textContent.indexOf('.') != -1) {
                 screen.textContent = result += e.target.id;
-                console.log(result);
-	    	}
-
-	    	/*if ( (action == null) || (b2 != null) ) {
-	    		a = result;
-	    	} else {
-	    		b = result;
-	    	}*/
-	    	console.log(a);
-	    	console.log(b);
-
+            }
+            assignAorB();
 	    	break;
 	    case 'dot':
 		    backspaceIsAllowed = true;
-		    //b2 = null;
-
 		    if (cleanScreen == true) {
 		    	screen.textContent = result = '0.'
     			cleanScreen = false;
     		} else if (screen.textContent.indexOf('.') == -1) {
 		    	screen.textContent = result += '.';
 		    }
-
-		    /*if ( (action == null) || (b2 != null) ) {
-	    		a = result;
-	    	} else {
-	    		b = result;
-	    	}*/
-
-		    console.log(result);
+			assignAorB();
 		    break;
 	    case 'sign':
 		    backspaceIsAllowed = true;
-
 		    if (screen.textContent != '0' && screen.textContent[0] != '-') {
-		    	//if (mainScreen.textContent.length >= 12) mainScreen.style.fontSize = '56px';
 		    	screen.textContent = result = '-' + result;
 		    } else if (screen.textContent != '0' && screen.textContent[0] == '-') {
-		    	//if (mainScreen.textContent.length > 12) mainScreen.style.fontSize = '58px';
-				screen.textContent = result = (result + '').slice(1);
+		    	screen.textContent = result = (result + '').slice(1);
 			}
-
-			/*if ( (action == null) || (b2 != null) ) {
-	    		a = result;
-	    	} else {
-	    		b = result;
-	    	}*/
-
-		    console.log(result);
+			assignAorB();
 		    break;
 	    case 'backspace':
 		    if (backspaceIsAllowed == true) {
@@ -100,14 +65,7 @@ calculator.addEventListener('click', function(e) {
 			    } else {
 				    screen.textContent = result = (result + '').slice(0, result.length - 1);
 			    }
-			    //if (screen.textContent.length > 12) screen.style.fontSize = '58px';
-			    /*if (action == null) {
-		    		a = result;
-		    	} else {
-		    		b = result;
-		    	}*/
-		    }
-		    console.log(result);
+			}
 		    break;
 	    case 'c':
 		    backspaceIsAllowed = true;
@@ -117,7 +75,6 @@ calculator.addEventListener('click', function(e) {
 		    b2 = null;
 		    action = null;
 		    cleanScreen = true;
-		    //actionsScreen.textContent = '';
 		    break;
 	    case 'm_plus':
 		    backspaceIsAllowed = false;
@@ -128,76 +85,32 @@ calculator.addEventListener('click', function(e) {
 		    backspaceIsAllowed = false;
 		    screen.textContent = result = memory;
 		    cleanScreen = true;
-
-		    /*if ( (action == null) || (b2 != null) ) {
-		    		a = result;
-		    	} else {
-		    		b = result;
-		    	}*/
-
-		    console.log(result);
+			assignAorB();
 		    break;
 	    case 'root':
 		    backspaceIsAllowed = false;
 		    cleanScreen = true;
-		    /*if (actionsScreen.textContent == '') {
-			    actionsScreen.textContent = `sqrt(${mainScreen.textContent})`;
-		    } else {
-		    	actionsScreen.textContent = `sqrt(${mainScreen.textContent})`;
-		    }*/
-		    result = '' + root(screen.textContent);
-		    if (result >= 0.99999999999 && result <= 1.00000000001) {
+		    result = root(+screen.textContent);
+		    if ( result >= 0.99999999999 && result <= 1.00000000001) {
 		    	screen.textContent = result = 1;
 		    	break;
-	    	}
-		    /*if (result.length > 13) {
-		    	//console.log(result.substr(0, 13));
-		    	screen.textContent = (result + '').substr(0, 13);
-		    }*/ else {
+	    	} else {
 		    	screen.textContent = result;
 		    }
-
-		    /*if ( (action == null) || (b2 != null) ) {
-	    		a = result;
-	    	} else {
-	    		b = result;
-	    	}*/
-
-		    //if (result >= 0.99999999999 && result <= 1.00000000001) screen.textContent = result = 1;
-		    console.log(result);
+			assignAorB();
 		    break;
 	    case 'fraction':
 		    backspaceIsAllowed = false;
 		    cleanScreen = true;
-		    //actionsScreen.textContent = `fraction(${mainScreen.textContent})`;
 		    screen.textContent = result = '' + fraction(result);
-		    /*if (result.length > 13) {
-		    	//console.log(result.substr(0, 13));
-		    	screen.textContent = (result + '').substr(0, 13);
-		    } else {
-		    	screen.textContent = result;
-		    }*/
-		    /*if ( (action == null) || (b2 != null) ) {
-	    		a = result;
-	    	} else {
-	    		b = result;
-	    	}*/
-
-		    console.log(result);
+		    assignAorB();
 		    break;
 	    case 'sum':
 	    case 'subtract':
 	    case 'multiply':
 	    case 'divide':
-		    /*if (action == null || firstPress == false) {
-		    	a = result;
-		    	firstPress = true;
-		    } else {
-		    	b = result;
-		    	screen.textContent = a = result = action(+a, +b);
-		    }*/
 		    if (b != null) {
-		    	screen.textContent = /*a =*/ result = action(+a, +b);
+		    	screen.textContent = a = result = action(+a, +b);
 		    	b = null;
 		    }
 		    action = (e.target.id == 'sum') ? sum: 
@@ -206,66 +119,160 @@ calculator.addEventListener('click', function(e) {
 		    b2 = null;
 		    backspaceIsAllowed = false;
 	    	cleanScreen = true;
-	    	console.log(action);
 	    	break;
 	    case 'percent':
 		    backspaceIsAllowed = false;
 		    cleanScreen = true;
 		    if (action == null) {
-		    	screen.textContent = /*a =*/ result = '0';
+		    	screen.textContent = a = result = '0';
 		    	break;
 		    } 
 		    screen.textContent = result = '' + ( +a * +screen.textContent/100 );
-		    /*if ( (action == null) || (b2 != null) ) {
-	    		a = result;
-	    	} else {
-	    		b = result;
-	    	}*/
+		    assignAorB();
 		    break;
     	case 'result':
-	    	/*console.log(a);
-	    	if (firstPress == true) {
-	    		b = result;
-	    		firstPress = false;
-	    	}
-	    	if (action != null && b != null) screen.textContent = a = result = action(+a, +b);
-	    	backspaceIsAllowed = false;
-	    	cleanScreen = true;*/
-	    	//action = null;
 	    	if (action != null) {
-	    		//console.log('kyky');
 	    		if (b2 == null) {
 	    			b2 = screen.textContent;
 	    			b = null;
-	    			//console.log(typeof b2);
 	    		}
-	    		
-	    		screen.textContent = /*a =*/ result = action(+a, +b2);
+	    		screen.textContent = a = result = action(+a, +b2);
 	    		backspaceIsAllowed = false;
 		    	cleanScreen = true;
 	    	}
 	    	break;
-
 	}
+	makeResultLength();
+});
 
+document.onkeypress = (e) => {
+	switch(e.key) {
+    	case '0':
+    	case '1':
+    	case '2':
+    	case '3':
+    	case '4':
+    	case '5':
+    	case '6':
+    	case '7':
+    	case '8':
+    	case '9':
+	    	backspaceIsAllowed = true;
+	    	if (cleanScreen == true || screen.textContent == '0') {
+	    		screen.textContent = result = e.key;
+	    		cleanScreen = false;
+	    	} else if (screen.textContent.length < 12) {
+	    		screen.textContent = result += e.key;
+	    	} else if (screen.textContent.length == 12 && 
+			    	  (screen.textContent[0] == '-' || screen.textContent.indexOf('.') != -1) ) {
+	    		screen.textContent = result += e.key;
+	    	} else if (screen.textContent.length == 13 && 
+	    		       screen.textContent[0] == '-' && screen.textContent.indexOf('.') != -1) {
+                screen.textContent = result += e.key;
+            }
+            assignAorB();
+	    	break;
+	    case '.':
+		    backspaceIsAllowed = true;
+		    if (cleanScreen == true) {
+		    	screen.textContent = result = '0.'
+    			cleanScreen = false;
+    		} else if (screen.textContent.indexOf('.') == -1) {
+		    	screen.textContent = result += '.';
+		    }
+			assignAorB();
+		    break;
+        case '+':
+	    case '-':
+	    case '*':
+	    case '/':
+		    if (b != null) {
+		    	screen.textContent = a = result = action(+a, +b);
+		    	b = null;
+		    }
+		    action = (e.key == '+') ? sum: 
+					 (e.key == '-') ? subtract: 
+					 (e.key == '*') ? multiply: divide;
+		    b2 = null;
+		    backspaceIsAllowed = false;
+	    	cleanScreen = true;
+	    	break;
+	    case 'Enter':
+	    	if (action != null) {
+	    		if (b2 == null) {
+	    			b2 = screen.textContent;
+	    			b = null;
+	    		}
+	    		screen.textContent = a = result = action(+a, +b2);
+	    		backspaceIsAllowed = false;
+		    	cleanScreen = true;
+	    	}
+	    	break;
+	}
+	makeResultLength();
+};
+
+document.onkeydown = (e) => {
+	switch(e.key) {
+		case 'Backspace':
+		    if (backspaceIsAllowed == true) {
+			    if (screen.textContent.length == 1 || (screen.textContent.length == 2 && screen.textContent[0] == '-') ) {
+			    	screen.textContent = result = 0;
+			    } else {
+				    screen.textContent = result = (result + '').slice(0, result.length - 1);
+			    }
+			}
+		    break;
+	}
+	makeResultLength();
+}
+
+function makeResultLength() {
+	let i = 11;
+	let j = 8;
+    if (+result >= 0) {
+    	screen.style.fontSize = '58px';
+    	if (+result < 1 && screen.textContent.length > 13) {
+    		while(screen.textContent.length > 13) {
+	    		screen.textContent = (+result).toPrecision(i);
+	    		i--;
+	    	}
+    	} else if (+result > 1 && +result < 999999999999 && screen.textContent.length > 13) {
+    		screen.textContent = (+result).toPrecision(12);
+    	} else if (+result > 999999999999) {
+    		while(screen.textContent.length > 13) {
+	    		screen.textContent = (+result).toPrecision(j);
+	    		j--;
+	    	}
+    	}
+    }
+    if (+result < 0) {
+    	if (+result > -1 && screen.textContent.length >= 14) {
+    		screen.style.fontSize = '54px';
+    		while(screen.textContent.length > 14) {
+	    		screen.textContent = (+result).toPrecision(i);
+	    		i--;
+	    	}
+    	} else if (+result < -1 && +result > -999999999999 && screen.textContent.length >= 14) {
+    		screen.style.fontSize = '54px';
+    		screen.textContent = (+result).toPrecision(12);
+    	} else if (+result < -999999999999) {
+    		console.log(result);
+    		while(screen.textContent.length > 13) {
+	    		screen.textContent = (+result).toPrecision(j);
+	    		j--;
+	    	}
+    	}
+    }
+ }
+
+function assignAorB() {
 	if ( (action == null) || (b2 != null) ) {
 		a = result;
 	} else {
 		b = result;
 	}
-
-    if (screen.textContent.length > 12 && screen.textContent[0] == '-' && screen.textContent.indexOf('.') != -1) {
-    	screen.textContent = (result + '').substr(0, 14);
-    	screen.style.fontSize = '54px';
-    } else if (screen.textContent.length > 12 && (screen.textContent[0] == '-' || screen.textContent.indexOf('.') != -1) ) {
-    	screen.textContent = (result + '').substr(0, 13);
-    	screen.style.fontSize = '58px';
-    } else if (screen.textContent.length > 12) {
-    	screen.textContent = (result + '').substr(0, 13);
-    	screen.style.fontSize = '58px';
-    }
-    
-});
+}
 
 function sum(a, b) {
 	return a + b;
